28c11a6a6a8c1738a5af9276f2446d73d190d3b4
Baal28 <107602112+Baal28@users.noreply.github.com>
2025-10-15 00:52:32 -0400

    modified GAME & DISPLAY CONTROLLERS

--- js/script.js
+++ js/script.js
@@ -37,9 +37,26 @@ const GAME_CONTROLLER = (function () {
     const playerX = createPlayer('Zakk', 'X');
     const playerO = createPlayer('Myles', 'O');
     let currentPlayer = playerX;
+    let gameStatus = true;
+    const WIN_COMBINATIONS = [
+        //Rows  //Columns //Diagonal
+        [0,1,2], [0,3,6], [0,4,8],
+        [3,4,5], [1,4,7], [2,4,6]
+        [6,7,8], [2,5,8],     
+    ]; 
     // Private Functions
     function checkForWin() {
-        //logic here
+        //get the current board state
+        const board = GAMEBOARD.getBoard();
+        //Loop and check for wins
+        for (const combination of WIN_COMBINATIONS) {
+            const [a, b, c] = combination;
+
+            if (board[a] !== '' && board[a] === board[b] && board[a] === board[c]) {
+                return true;
+            }
+        }
+        return false;
     }
     // The function Must return an object
     return{
@@ -59,13 +76,28 @@ const GAME_CONTROLLER = (function () {
             }  
         },
         playerChoice: function(index){
+            if (gameStatus === false) {
+                return;
+            }
             const marker = currentPlayer.marker;
             const moveSuccesful = GAMEBOARD.markSpot(index,marker);
             //checks if markSpot returned true so it can change turns.
             if (moveSuccesful === true) {
-                GAME_CONTROLLER.switchTurn();
                 DISPLAY_CONTROLLER.updateDisplay();
-            }
+
+                const isWin = checkForWin();
+                const isDraw = !GAMEBOARD.getBoard().includes('');
+
+                if (isWin) {
+                    gameStatus = false;
+                    return DISPLAY_CONTROLLER.displayMessage(`${currentPlayer.name} Wins!`)
+                } else if (isDraw) {
+                    gameStatus = false;
+                    return DISPLAY_CONTROLLER.displayMessage(`Game Over: Draw!`)
+                } else {
+                    GAME_CONTROLLER.switchTurn();
+                }
+            };
         },
     };
 })();
@@ -74,6 +106,18 @@ const GAME_CONTROLLER = (function () {
 const DISPLAY_CONTROLLER = (function () {
     //Private state goes here
     let boardContainer = document.querySelector('.gameboard-container');
+
+    //Private Functions below
+    function handleClick(e) {
+        let index = e.target.dataset.index;
+        GAME_CONTROLLER.playerChoice(index);
+    }
+
+    function setupEventListeners() {
+        boardContainer.addEventListener('click', handleClick);
+    }
+    setupEventListeners();
+
     //The function MUST return an object {}
     return{
         updateDisplay: function () {
@@ -92,5 +136,8 @@ const DISPLAY_CONTROLLER = (function () {
                 boardContainer.appendChild(cellElement);
             });
         },
+        displayMessage: function (message){
+            alert(message);
+        }
     }
 })();
